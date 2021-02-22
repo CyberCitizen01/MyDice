47bca7bd6534a81de62eef15d08d40e5c3989f05
CyberCitizen01 <mr.20u10448@btech.nitdgp.ac.in>
2021-02-22 21:13:43 +0530

    Complete 2 player game
    anyone who comes first and starts the game at http://localhost:3000 is the first user and the someone who is already
    or who comes after game has started is notified of starting of the game and thus he can also participate in it by clicking
    GO. After that any of the two can either replay or replay by changing their name or just perform a rematch(rematch is in progress).
    Replay, simply make new guesses and replay by changing name option starts the game but let's you change your name.
    Note that, once a game starts a user cannot change his/her name until the game has ended, i.e. until the replay by changing button appears.
    This branch contains two-player game but the main branch will contain Multiplayer(Proposed to be at most 5)(still in progress as of now).

--- server/server.js
+++ server/server.js
@@ -12,58 +12,110 @@ let io = socketIO(server);
 
 app.use(express.static(publicpath));
 
-var User1 = {
+var User1 ={
     Name: '',
-    Num:0
-};
-var User2 = {
+    Num: 0
+}
+var User2 ={
     Name: '',
-    Num:0
+    Num: 0
+}
+var WIN = {
+    Name: '',
+    Num:0,
+    User1: User1,
+    User2: User2
 };
+var u1 = '1', u2 = '1';
+var state = 'start';
+var chUser='';
 
-var i = 0;
+function maxGen(U1, U2){
+    var s1=U1;
+    var s2=U2;
 
-//console.log(WIN.Name == 'k' );
-//console.log(WIN.Num);
+    User1.Name=s1;
+    User1.Num=Math.floor((Math.random() * 6) + 1);
+    User2.Name=s2;
+    User2.Num=Math.floor((Math.random() * 6) + 1);
+
+    if(User1.Num==User2.Num){
+        maxGen(s1, s2);
+    }
+    else if(User1.Num!=User2.Num){
+        WIN.Num = Math.max(User1.Num, User2.Num);
+        if(WIN.Num == User1.Num && WIN.Num != User2.Num){
+            WIN.Name = User1.Name;
+        }
+        else if(WIN.Num == User2.Num && WIN.Num != User1.Num){
+            WIN.Name = User2.Name;
+        }
+    }
+};
 
 io.on('connection',function(socket){
-    console.log('You are connected to the client');
+    socket.on('Cache',function(){
+        if(u1!='1'){
+            socket.emit('call',u1)
+        }
+    })
+    console.log('You are connected to the client!!!');
     socket.on('myNum', function(data){
-        console.log(data);
-        io.emit('yourNum',data);
-        if(i==0){
-            User1 = data;
-            console.log('User1: ',User1);
-            i++;
-            io.emit('call',User1);
+        socket.emit('yourNum',data);
+        if(state=='start'){
+            u1 = data;
+            console.log('User1: ',u1);
+            state='User1 started';
+            io.emit('call',u1);
         }
-        else{
-            User2 = data;
-            console.log('User2: ',User2);
-            i--;
-            var WIN = {
-                Name: '',
-                Num:0,
-                User1: User1,
-                User2: User2
-            };
-            WIN.Num = Math.max(User1.Num, User2.Num);
-            if(WIN.Num == User1.Num && WIN.Num != User2.Num){
-                WIN.Name = User1.Name;
+        else if(state=='User1 started'){
+            u2 = data;
+            console.log('User2: ',u2);
+            state='win';
+            if(state=='win'){
+                maxGen(u1, u2);
+                io.emit('winner',WIN);
+                state='start'
             }
-            else if(WIN.Num == User2.Num && WIN.Num != User1.Num){
-                WIN.Name = User2.Name;
-            }
-            else{
-                io.emit('re-evaluate');
-            }
-            io.emit('winner',WIN);
         }
+        });
+
+    socket.on('replay-msg',function(data){
+        if(data==u1 && data!=u2){
+            u1=data;
+            maxGen(u1, u2);
+            io.emit('replay-msg-a',u1);
+        }
+        else if(data==u2 && data!=u1){
+            u2=data;
+            maxGen(u1, u2);
+            io.emit('replay-msg-a',u2);
+        }
+        io.emit('winner',WIN);
     });
+
+    socket.on('NameChange',function(data){
+        chUser=data;
+        socket.broadcast.emit('NameReq');
+    })
+
+    socket.on('NameReq-ans',function(data){
+        if(data==u1 && data!=u2){
+            u2=chUser;
+            maxGen(u1, u2);
+            io.emit('replay-msg-a',u2);
+        }
+        else if(data==u2 && data!=u1){
+            u1=chUser;
+            maxGen(u1, u2);
+            io.emit('replay-msg-a',u1);
+        }
+        io.emit('winner',WIN);
+    })
 
     socket.on('disconnect',function(){
         console.log('You are disconnected to the client');
-    })
+    });
 })
 
 server.listen(port, function(){
